a3a2d3acd31676d4c45b81a79351d5b7f5418b62
Gfycyd <31628809+Gfycyd@users.noreply.github.com>
2018-05-28 06:26:58 +0300

    itoa

--- Assignment 1/src/task.c
+++ Assignment 1/src/task.c
@@ -267,4 +267,31 @@ void flush(){
         else putchar(c);
     }
 }
+char *itoa(int n, char s[], int w){
+void reverse(char s[]);
+
+int i,sign;
+if ((sign = n) < 0)
+n = -n;
+i = 0;
+do {
+s[i++] = n % 10 + '0';
+}
+while ((n /=10) > 0);
+if (sign <0)
+s[i++] = '-';
+while(i<w)
+s[i++]=' ';
+s[w] = '\0';
+reverse(s);
+return s;
+}
+void reverse(char s[])
+{
+    int i,j,c;
+
+    for(i=0,j=strlen(s)-1;i<j;i++,j--)
+        c=s[i],s[i]=s[j],s[j]=c;
+}
+char *itob(int n, const char s[], int b);
 /** GET FROM task.h */
