a147b4f3eba20aacd9017f69887fee254e4ee480
Gfycyd <31628809+Gfycyd@users.noreply.github.com>
2018-05-28 07:14:12 +0300

    Update task.c

--- Assignment 1/src/task.c
+++ Assignment 1/src/task.c
@@ -293,5 +293,24 @@ void reverse(char s[])
     for(i=0,j=strlen(s)-1;i<j;i++,j--)
         c=s[i],s[i]=s[j],s[j]=c;
 }
-char *itob(int n, const char s[], int b);
+char *itob(int n, const char s[], int b){
+}
+int binsearch( int x, int v[], int n){
+    int low, high, mid;
+    low = 0;
+    high = n -1;
+    mid = (low+high)/2;
+
+    while( low < high && x!=v[mid]){
+        if (x < v[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+        mid = (low+high)/2;
+    }
+    if (x == v[mid]){
+        return mid;
+    }
+    else return -1; /* no match */
+}
 /** GET FROM task.h */
