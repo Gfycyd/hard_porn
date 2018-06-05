76b8e4009dbd804ee50d357555e1f094f0a5f1e7
Rinatum <grinamul@gmail.com>
2018-06-05 15:44:13 +0300

    assignment 3(+asserts)

--- Assignment 1/src/assignment3/bitMap.c
+++ Assignment 1/src/assignment3/bitMap.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <assert.h>
 #include "bitMap.h"
 
 
@@ -9,20 +10,16 @@ void setBitByNumber(int* array, int bit, int position)
     // bit - have to be equal to 0 or 1
     //
     // this function changes bit with number 'position' to the 'bit'
-    if (position < sizeof(array)/ sizeof(int) && position >= 0)
-        if (bit == 0 || bit == 1)
-            array[0 + position] = bit;
-        else
-            exit(1);
+    assert(position < sizeof(array)/ sizeof(int) && position >= 0);
+    assert(bit == 0 || bit == 1);
+    array[0 + position] = bit;
 }
 
 int getBitByNumber(int* array, int position)
 {
     // this function returns position's bit from the array
-    if (position < sizeof(array)/ sizeof(int) && position >= 0)
-        return array[0 + position];
-    else
-        exit(1);
+    assert(position < sizeof(array)/ sizeof(int) && position >= 0);
+    return array[0 + position];
 }
 
 void setBitByAddress(void* position, int bit)
@@ -31,10 +28,8 @@ void setBitByAddress(void* position, int bit)
     // bit - have to be equal to 0 or 1
     //
     // this function changes bit by memory address to 'bit'
-    if (bit == 0 || bit == 1)
-        *(int*)position = bit;
-    else
-        exit(1);
+    assert(bit == 0 || bit == 1);
+    *(int*)position = bit;
 }
 
 int getBitByAddress(void* position)
